fb02898de32e27e315e204e8049f965680e5c315
lmnnarciso <lmnnarciso14@gmail.com>
2022-06-03 23:40:02 +0800

    nftcollection contract

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x8F8F1c5a7a3559e440cdca6B55e46aEb71F710cc";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelisting-dapp-ten.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
